77f2b653c432c82861e332b0a33fa44f44eb10a2
Xneg <xnegxneg@gmail.com>
2018-05-03 23:02:22 +0300

    * Change solution name and location. * Added empty C# consumer project.

---
+++ CSharp.Dll.Consumer/DllConsumer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CSharp.Dll.Consumer
+{
+    public class DllConsumer
+    {
+        [DllImport("SimpleDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int AddNumsReturn(int a, int b);
+    }
+}
